e9fdaca1b810113f46bc3db7c8524f0ec4686506
Dhananjay Kumar <dhananjaykjcs@gmail.com>
2023-07-30 21:49:48 +0530

    Finally done

---
+++ src/components/Body/Body.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Body.css";
+import QRCode from "qrcode.react";
+
+function Body() {
+  const [inputText, setInputText] = useState("");
+  const [qrCodeText, setQRCodeText] = useState("");
+
+  const generateQrCode = () => {
+    setQRCodeText(inputText);
+  };
+
+
+  const downloadQRCode = () => {
+    const qrCodeURL = document
+      .getElementById("qrCodeEl")
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    console.log(qrCodeURL);
+    let aEl = document.createElement("a");
+    aEl.href = qrCodeURL;
+    aEl.download = "QR_Code.png";
+    document.body.appendChild(aEl);
+    aEl.click();
+    document.body.removeChild(aEl);
+  };
+
+  
+
+  return (
+    <div className="body">
+      <span className="bodyTextToQR">
+        <input
+          type="text"
+          value={inputText}
+          className="bodyTextToQRtext"
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="Enter Text ..."
+        />
+        <button onClick={generateQrCode} className="bodyTextToQRbutton">
+          Generate QR
+        </button>
+      </span>
+      <div className="space">
+      <div className="bodyContainer">
+        <QRCode className="qrcode" id="qrCodeEl" size={200} value={qrCodeText} />
+
+        <button className="download" onClick={downloadQRCode}>
+          Download
+        </button>
+      </div>
+      </div>
+      <p>Created by D.J</p>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+
+         <h1 className="navbarText">QR CODE GENERATOR</h1>
+
+    </div>
+  );
+};
+
+export default Navbar;
